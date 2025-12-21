deb4a32b0f639e4dea0f43703ad13b919a25767e
Tobias Hollstein <tobias@git.com>
2025-12-21 05:53:48 +0100

    mvvm

--- src/mapped-array.js
+++ src/mapped-array.js
@@ -1,18 +1,35 @@
 // Mapped Array Utility: Create bidirectional array mappings for MVVM pattern
 
-export function createMappedArray(source, transform, writableProps = {}, reverseTransform = (x) => x) {
+export function createMappedArray(source, transform, writableProps = {}, reverseTransform = (x) => x, cachedProps = {}, options = {}) {
+    const { idProperty = 'id' } = options
+    
     const arr = source.map((item, index) => {
         const result = transform(item, index)
         
         // Add getter/setter for writable properties
         for (const [prop, sourceProp] of Object.entries(writableProps)) {
-            const value = result[prop]
             delete result[prop]
-            
+
             Object.defineProperty(result, prop, {
-                get: () => source[index][sourceProp],
-                set: (v) => { source[index][sourceProp] = v },
-                enumerable: true
+                get: () => {
+                    const targetItem = source.find(s => s[idProperty] === item[idProperty])
+                    return targetItem ? targetItem[sourceProp] : undefined
+                },
+                set: (v) => {
+                    const targetItem = source.find(s => s[idProperty] === item[idProperty])
+                    if (targetItem) targetItem[sourceProp] = v
+                }
+            })
+        }
+        
+        // Add getter/setter for cached properties (UI state)
+        for (const [prop, cache] of Object.entries(cachedProps)) {
+            const itemId = item.id
+            const defaultValue = result[prop] !== undefined ? result[prop] : false
+            delete result[prop]
+            Object.defineProperty(result, prop, {
+                get: () => cache[itemId] !== undefined ? cache[itemId] : defaultValue,
+                set: (v) => { cache[itemId] = v },
             })
         }
         
